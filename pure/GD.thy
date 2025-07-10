0eae526ea81c53e30dbf884482a30d185511ce66
Sascha Kehrli <skehrli@ethz.ch>
2025-07-10 10:02:13 +0200

    template for gd in pure

---
+++ pure/GD.thy
@@ -0,0 +1,20 @@
+﻿(* Title: pure/GD.thy
+*)
+
+section \<open>Grounded deduction logic\<close>
+
+theory GD
+  imports Pure
+begin
+
+text \<open>The following theory development formalizes the Grounded Deduction Logic.
+      Isabelle/Isar.\<close>
+
+section \<open>GD syntax and axiomatization\<close>
+
+typedecl o
+
+judgment
+  Trueprop :: \<open>o \<Rightarrow> prop\<close>
+
+end
